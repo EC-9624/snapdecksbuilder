d3d0429682087632853985a6b2f149237ecd7241
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-02-17 13:56:22 +0900

    Data reading clean up Display Cardsdata

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Card = ({ data }) => {
-  const { art, ability } = data;
+const Card = (data) => {
+  console.log(data);
   return (
     <div className="w-56 h-96 bg-slate-500 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <img className="rounded-t-lg" src={art} alt="" />
-      <div className="p-2">
-        <p className="mb-3 font-normal text-gray-900 dark:text-gray-400">
-          {ability}
+      <img className="rounded-t-lg" src={data.data.art} alt="" />
+      <div>
+        <p className="mb-3 font-normal text-gray-900 dark:text-gray-400 text-center">
+          {data.data.ability}
         </p>
       </div>
     </div>
