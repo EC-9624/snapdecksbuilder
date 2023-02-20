36939345082935a45cdf34abeed45b752deb5e01
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-02-20 14:05:26 +0900

    styling builder

--- src/components/builder.jsx
+++ src/components/builder.jsx
@@ -3,21 +3,19 @@ import Card from "./card";
 
 function Builder() {
   return (
-    <div>
-      <div className=" w-2/4 h-auto bg-slate-400 flex flex-wrap">
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-        <div className="w-40 h-52 bg-black m-5"></div>
-      </div>
+    <div className="grid grid-cols-6 grid-rows-2 gap-4 border-2 border-red-500 p-5">
+      <div className="bg-gray-100 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-gray-200 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-gray-300 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-gray-400 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-gray-500 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-gray-600 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-gray-700 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-orange-300 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-gray-900 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-red-500 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-green-500 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
+      <div className="bg-blue-500 h-32 w-24 border-solid border-4 border-yellow-500 rounded "></div>
     </div>
   );
 }
--- src/pages/builder/index.jsx
+++ src/pages/builder/index.jsx
@@ -3,12 +3,15 @@ import Header from "@/components/header";
 import Builder from "@/components/builder";
 const builder = () => {
   return (
-    <div className="bg-slate-800 min-h-screen ">
-      <Header></Header>
-      <Builder></Builder>
-      <h2>Filtering Component</h2>
-      <h2>Cards</h2>
-    </div>
+    <>
+      <div className="bg-slate-800 container min-h-screen w-screen flex flex-col justify-center items-center">
+        <Header></Header>
+        <div>
+          <Builder></Builder>
+        </div>
+        <div className="section-2 text-white">Section 2</div>
+      </div>
+    </>
   );
 };
 
